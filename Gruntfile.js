69a13b79cf86aade576d0d3f81f1e553d13639c9
Nikitenko Gennadiy <nikitenkogv.main@gmail.com>
2017-04-03 18:26:19 +0300

    Убрана автоматизация

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,62 +5,12 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-postcss");
   grunt.loadNpmTasks("grunt-sass");
-  grunt.loadNpmTasks("grunt-css-mqpacker");
-  grunt.loadNpmTasks("grunt-csso");
-  grunt.loadNpmTasks("grunt-contrib-imagemin");
-  grunt.loadNpmTasks("grunt-contrib-copy");
-  grunt.loadNpmTasks("grunt-contrib-clean");    
-   
 
   grunt.initConfig({
-      clean: {
-          build: ["build"]
-      },
-      copy: {
-          build: {
-              files: [{
-                  expand: true,
-                  src: [
-                      "fonts/**/*.{woff,woff2}",
-                      "img/**",
-                      "js/**",
-                      "*.html"
-                  ],
-                  dest: "build"
-              }]
-          },
-          html: {
-              files: [{
-                  expand: true,
-                  src: ["*.html"],
-                  dest: "build"
-              }]
-          }
-      },
-      imagemin: {
-          images: {
-              options: {
-                  optimizationLevel: 3,
-                  progressive: true
-              },
-              files: [{
-                  expand: true,
-                  src: ["build/img/**/*.{png,jpg,gif}"]
-              }]
-          }
-      },
-      csso: {
-          style: {
-              report: "qzip"
-          },
-          files: {
-              "build/css/style.min.css": ["build/css/style.css"]
-          }
-      },
     sass: {
       style: {
         files: {
-          "build/css/style.css": "sass/style.scss"
+          "css/style.css": "sass/style.scss"
         }
       }
     },
@@ -71,11 +21,10 @@ module.exports = function(grunt) {
           processors: [
             require("autoprefixer")({browsers: [
               "last 2 versions"
-            ]}),
-              require("css-mqpacker")({sort: true})
+            ]})
           ]
         },
-        src: "build/css/*.css"
+        src: "css/*.css"
       }
     },
 
@@ -83,12 +32,12 @@ module.exports = function(grunt) {
       server: {
         bsFiles: {
           src: [
-            "build*.html",
-            "build/css/*.css"
+            "*.html",
+            "css/*.css"
           ]
         },
         options: {
-          server: "build/",
+          server: ".",
           watchTask: true,
           notify: false,
           open: true,
@@ -99,24 +48,12 @@ module.exports = function(grunt) {
     },
 
     watch: {
-        html:{ 
-        files: ["*.html"],
-        tasks: ["copy:html"] },
       style: {
         files: ["sass/**/*.{scss,sass}"],
-        tasks: ["sass", "postcss", "csso"]
+        tasks: ["sass", "postcss"]
       }
     }
   });
 
   grunt.registerTask("serve", ["browserSync", "watch"]);
-    
-  grunt.registerTask("build", [
-      "clean",
-      "copy",
-      "sass",
-      "postcss",
-      "csso",
-      "imagemin"
-  ]);    
 };
